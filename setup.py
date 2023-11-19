47798869fd47cb1567c1cf44810fde3424284047
Christopher Lovell <15553561+gmfCoding@users.noreply.github.com>
2023-11-20 02:45:42 +1030

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name = "xpm",
-    version = "0.3",
-    author = "Tal Wrii",
-    author_email = "talwrii@gmail.com",
+    version = "0.3.1",
+    author = "Tal Wrii, Christopher Lovell",
+    author_email = "talwrii@gmail.com, N/A",
     description = "Library to output XPM files",
     license = "BSD",
     keywords = "xpm image PIL graphics",
-    url = "https://github.com/talwrii/xpm",
+    url = "https://github.com/gmfCoding/xpm_argb",
     py_modules=['xpm'],
     classifiers=[
         "Development Status :: 3 - Alpha",
